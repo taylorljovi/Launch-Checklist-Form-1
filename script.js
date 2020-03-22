d4a6a69a203a914d1bc205168b3e0db2fec485cc
unknown <taylorljovi@gmail.com>
2020-03-22 16:21:12 -0500

    working on second part

--- script.js
+++ script.js
@@ -13,17 +13,62 @@
 */
 window.addEventListener("load", function(){
    let form = document.querySelector("form");
-   form.addEventListener("submit", function(event) {
-      let pilotName = document.querySelector("input[name=pilotName]");
-      let copilotName = document.querySelector("input[name=copilotName]");
-      let fuelLevel = document.querySelector("input[name=fuelLevel]");
-      let cargoMass = document.querySelector("input[name=cargoMass]");
+   let pilotName = document.querySelector("input[name=pilotName]");
+   let copilotName = document.querySelector("input[name=copilotName]");
+   let fuelLevel = document.querySelector("input[name=fuelLevel]");
+   let cargoMass = document.querySelector("input[name=cargoMass]");
+  
+   form.addEventListener("submit", inputCheck);
+
+   form.addEventListener("load", launchStatusCheck);
+   
+   function inputCheck(event){
       if (pilotName.value === "" || copilotName.value === ""){
-         alert ("All feilds are required!");
+         alert ("All fields are required!");
          event.preventDefault();
       } else if (isNaN(fuelLevel.value) || isNaN(cargoMass.value) ){
-         alert ("All feilds are required");
+         alert ("All fields are required");
          event.preventDefault();
       }
-   })
-})
+   }
+
+
+let faultyItems = document.getElementById("faultyItems");
+funciton fuelStatus(){
+   let fuelLevelStatus;
+   if(Number(fuelLevel.value) < 10000){
+      fuelLevelStatus =`<li id="fuelStatus">Fuel level not high enough for launch</li>`;
+   } 
+   else if (Number(fuelLevel.value) >= 10000){
+      fuelLevelStatus = `<li id="fuelStatus">Fuel level is high enough for launch</li>`;
+   } 
+   return fuelLevelStatus;
+   }
+
+   function cargoStatus(){
+      let cargoStatus;
+      if (Number(cargoMass.value) > 10000) {
+         cargoStatus = `<li id="cargoStatus">Cargo Mass low enough for launch</li>`;
+      }
+      else if (Nubmer(cargoMass.value) <= 10000) {
+         cargoStatus = `<li id="cargoStatus">Cargo Mass not low enough for launch</li>`;
+      }
+      return cargoStatus;
+   }
+function launchStatusCheck(){
+   if (Number(fuelLevel.value) < 10000 || Number(cargoMass) > 10000){
+      document.getElementById("launchStatus").innerHTML = `<font color = "red"> Shuttle not ready for launch</font>`;
+      faultyItems.setAttribute("visability","visibile");
+      faultyItems.innerHTML += 
+       `<ol>
+          <li id="pilotStatus">${pilotName.value} Ready</li>
+          <li id="copilotStatus">${copilotName.value} Ready</li>
+          ${fuelStatus()} </li>
+          ${cargoStatus()}</li>
+       </ol>`;
+   }
+   else {
+      document.getElementById("launchStatus").innerHTML = `font color="greet"> Ready for launch</font:`;
+   }
+ }
+});
