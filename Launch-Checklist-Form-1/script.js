c8b21021f6eedd484d348f10847f03fbbd9c2b90
unknown <taylorljovi@gmail.com>
2020-04-16 11:47:14 -0500

    final submit

--- Launch-Checklist-Form-1/script.js
+++ Launch-Checklist-Form-1/script.js
@@ -17,21 +17,23 @@ window.addEventListener("load", function() {
          <img src="${json[index].image}"></img>`
       })
    })
-
    let form = document.querySelector("form");
-   let pilotName = document.querySelector("input[name=pilotName]");
-   let copilotName = document.querySelector("input[name=copilotName]");
-   let fuelLevel = document.querySelector("input[name=fuelLevel]");
-   let cargoMass = document.querySelector("input[name=cargoMass]");
-  
    form.addEventListener("submit", inputCheck);
-  
+   
    function inputCheck(event) {
+      let form = document.querySelector("form");
+      let pilotName = document.querySelector("input[name=pilotName]");
+      let copilotName = document.querySelector("input[name=copilotName]");
+      let fuelLevel = document.querySelector("input[name=fuelLevel]");
+      let cargoMass = document.querySelector("input[name=cargoMass]");
+      let numFuelLevel = (Number(fuelLevel.value));
+      let numCargoMass = (Number(cargoMass.value));
+
       event.preventDefault();
       if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass === "") {
          alert ("All fields are required!");  
       } 
-      else if (isNaN(fuelLevel.value) || isNaN(cargoMass.value)){
+      else if (isNaN(numFuelLevel) || isNaN(numCargoMass)){
          alert ("Make sure to enter valid information for each field!");
       }
       else {
@@ -40,7 +42,15 @@ window.addEventListener("load", function() {
    }
 
 let faultyItems = document.getElementById("faultyItems");
+
 function fuelStatus() {
+   let form = document.querySelector("form");
+   let pilotName = document.querySelector("input[name=pilotName]");
+   let copilotName = document.querySelector("input[name=copilotName]");
+   let fuelLevel = document.querySelector("input[name=fuelLevel]");
+   let cargoMass = document.querySelector("input[name=cargoMass]");
+   let numFuelLevel = (Number(fuelLevel.value));
+   let numCargoMass = (Number(cargoMass.value));
    let fuelLevelStatus;
    if(Number(fuelLevel.value) < 10000) {
       fuelLevelStatus =`<li id="fuelStatus">Fuel level not high enough for launch</li>`;
@@ -52,6 +62,13 @@ function fuelStatus() {
 }
 
 function cargoStatus(){
+   let form = document.querySelector("form");
+   let pilotName = document.querySelector("input[name=pilotName]");
+   let copilotName = document.querySelector("input[name=copilotName]");
+   let fuelLevel = document.querySelector("input[name=fuelLevel]");
+   let cargoMass = document.querySelector("input[name=cargoMass]");
+   let numFuelLevel = (Number(fuelLevel.value));
+   let numCargoMass = (Number(cargoMass.value));
    let cargoStatus;
    if (Number(cargoMass.value) > 10000) {
      cargoStatus = `<li id="cargoStatus">Cargo Mass low enough for launch</li>`;
@@ -63,6 +80,13 @@ function cargoStatus(){
 }
 
 function launchStatusCheck(){
+   let form = document.querySelector("form");
+   let pilotName = document.querySelector("input[name=pilotName]");
+   let copilotName = document.querySelector("input[name=copilotName]");
+   let fuelLevel = document.querySelector("input[name=fuelLevel]");
+   let cargoMass = document.querySelector("input[name=cargoMass]");
+   let numFuelLevel = (Number(fuelLevel.value));
+   let numCargoMass = (Number(cargoMass.value));
    event.preventDefault();
    if (Number(fuelLevel.value) < 10000 || Number(cargoMass) > 10000){
       document.getElementById("launchStatus").innerHTML = `<font color = "red">Shuttle not ready for launch</font>`;
